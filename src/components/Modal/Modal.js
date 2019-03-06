985b7becd0df3c2e6c21a6dd22bc87207645f6f3
oahehc <oahehc@gmail.com>
2019-03-06 22:11:41 +0800

    project init

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './Modal.css';
+
+export default class Modal extends Component {
+  static propTypes = {};
+  static defaultProps = {};
+
+  render() {
+    return (
+      <div>
+        modal
+      </div>
+    );
+  }
+}
+
